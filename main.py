76846f257096cd261c416937119faa1578d31e40
eyaelEskinder <eyaelEskinder@gmail.com>
2023-09-05 13:35:35 +1000

    ck + javaparser

--- main.py
+++ main.py
@@ -16,54 +16,101 @@ def home():
         current_dir = os.path.join("targ_files", current_id)
         os.mkdir(current_dir)
 
+        # save and extract project
         pro_file = request.files['file']
         temp_file_path = os.path.join(current_dir, pro_file.filename)
         pro_file.save(temp_file_path)
+        with zipfile.ZipFile(temp_file_path, 'r') as zip_ref:
+            zip_ref.extractall(current_dir)
 
-        lib_provided = False
-        try:
-            lib_files = request.files['libFiles']
-            lib_file_path = os.path.join(current_dir, 'lib.zip')
-            lib_files.save(lib_file_path)
-            with zipfile.ZipFile(lib_file_path, 'r') as zip_ref:
-                zip_ref.extractall(current_dir)
-                lib_provided = True
-        except:
-            pass
+        proj_folder = os.path.join(current_dir, pro_file.filename[:-4])
 
-        ckjm_output = os.path.join(current_dir, "ckjm_output.txt")
-        ckjm_command = ["java", "-jar", "runable-ckjm_ext-2.5.jar", temp_file_path]
+        # compile
+        compile_output = os.path.join(proj_folder, "compile_output")
+        os.mkdir(compile_output)
+        java_source_dir = f"{proj_folder}/src"
+        dependencies = f"{proj_folder}/lib/*"
+
+        java_files = []
+        for root, _, files in os.walk(java_source_dir):
+            for file in files:
+                if file.endswith(".java"):
+                    java_files.append(os.path.join(root, file))
+
+        if java_files:
+            # try:
+            with cjdk.java_env(vendor="temurin", version="8.0.372"):
+                subprocess.run(["javac", "-cp", dependencies, "-d", compile_output] + java_files,
+                               check=True)
+                print("Compilation successful!")
+            # except subprocess.CalledProcessError as e:
+            #     print(f"Compilation failed: {e}")
+        else:
+            print(f"No Java files found")
+
+        class_files = subprocess.check_output(['find', compile_output, '-name', '*.class'], text=True).split('\n')
+        class_files = class_files[:-1]
+
+        # ck
+        ck_output = os.path.join(current_dir, "ck_output")
+        os.mkdir(ck_output)
+        use_jars = 'true'
+        max_files_per_partition = '0'
+        variables_and_field_metrics = 'False'
+        ck_command = ["java", "-jar", "ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar", proj_folder, use_jars,
+                      max_files_per_partition, variables_and_field_metrics, ck_output + "/"]
         with cjdk.java_env(vendor="temurin-jre", version="17.0.3"):
-            with open(ckjm_output, "w") as output_file:
-                subprocess.run(ckjm_command, stdout=output_file, check=True)
+            subprocess.run(ck_command, check=True)
 
-        with open(os.path.join(current_dir, "ckjm_output.csv"), 'w') as ckjm_csv:
+        # javaparser
+        jp_output = os.path.join(current_dir, "jp_output")
+        os.mkdir(jp_output)
+        jp_command = ["java", "-jar", "javaparser-extractor-1.0-SNAPSHOT-shaded.jar", proj_folder, jp_output + "/"]
+        with cjdk.java_env(vendor="temurin-jre", version="18.0.1"):
+            subprocess.run(jp_command, check=True)
+
+        # ckjm
+        ckjm_output = os.path.join(current_dir, "ckjm_output")
+        os.mkdir(ckjm_output)
+        ckjm_text = os.path.join(ckjm_output, "ckjm_output.txt")
+
+        for class_file in class_files:
+            try:
+                ckjm_command = ["java", "-jar", "runable-ckjm_ext-2.5.jar", class_file]
+                with cjdk.java_env(vendor="temurin-jre", version="17.0.3"):
+                    with open(ckjm_text, "a") as output_file:
+                        subprocess.run(ckjm_command, stdout=output_file, check=True)
+            except:
+                pass
+
+        with open(os.path.join(ckjm_output, "ckjm_output.csv"), 'w') as ckjm_csv:
             writer = csv.writer(ckjm_csv)
             writer.writerow(("TARGET_CLASS", "WMC", "DIT", "NOC", "CBO", "RFC", "LCOM", "Ca", "Ce", "NPM", "LCOM3", "LOC",
                              "DAM", "MOA", "MFA", "CAM", "IC", "CBM", "AMC"))
-            with open(os.path.join(current_dir, "ckjm_output.txt")) as in_file:
+            with open(os.path.join(ckjm_output, "ckjm_output.txt")) as in_file:
                 stripped = (line.strip() for line in in_file)
                 lines = (line.split(" ") for line in stripped if line)
                 lines = (line for line in lines if line[0] != '~')
                 writer.writerows(lines)
 
-        # java8_home = "/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home"
-        # java8 = f"{java8_home}/bin/java"
-        # java8_tools = "/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home/lib/tools.jar"
 
-        # os.environ["JAVA_HOME"] = java8_home
-        # evosuite_comm = f"{java8} -jar evosuite-1.0.6.jar -target {temp_file_path} -criterion branch -Dreport_dir={current_dir} -Dtest_dir={current_dir}/tests -Dtools_jar_location={java8_tools} -Dshow_progress=false"
-        # if lib_provided:
-        #     evosuite_comm += f" -projectCP $(ls {current_dir}/lib/*.jar | tr '\n' ':')"
-        # os.system(evosuite_comm)
+        # evosuite
+        # evo_output = os.path.join(current_dir, "evo_output")
+        # os.mkdir(evo_output)
+        #
+        # evo_command = ["java", "-jar", "evosuite-1.0.6.jar", "-target", compile_output, "-criterion", "branch",
+        #                f"-Dreport_dir={evo_output}", f"-Dtest_dir={evo_output}/tests", "-Dshow_progress=false"]
+        #
+        # jar_files = [file for file in os.listdir(f"{proj_folder}/lib") if file.endswith('.jar')]
+        # if len(jar_files) != 0:
+        #     jar_path = ":".join(os.path.join(f"{proj_folder}/lib", jar) for jar in jar_files)
+        #     print("jar_path", jar_path)
+        #     evo_command.append("-projectCP")
+        #     evo_command.append(jar_path)
+        #
+        # with cjdk.java_env(vendor="temurin", version="8.0.372"):
+        #     subprocess.run(evo_command, check=True)
 
-        # evosuite_comm = f"  -Dtools_jar_location={java8_tools}"
-
-        evo_command = ["java", "-jar", "evosuite-1.0.6.jar", "-target", temp_file_path, "-criterion", "branch", f"-Dreport_dir={current_dir}", f"-Dtest_dir={current_dir}/tests", "-Dshow_progress=false"]
-        # # if lib_provided:
-        #     evo_command += f" -projectCP $(ls {current_dir}/lib/*.jar | tr '\n' ':')"
-        with cjdk.java_env(vendor="temurin", version="8.0.372"):
-            subprocess.run(evo_command, check=True)
 
         # try:
         #     data1 = pd.read_csv(f'{current_dir}/statistics.csv')
@@ -74,12 +121,14 @@ def home():
         # merged_output = pd.merge(data1, data2, on='TARGET_CLASS', how='outer')
         # merged_output.to_csv(f'{current_dir}/merged_output.csv')
 
-        data1 = pd.read_csv("targ_files/ckjm_output.csv")
-        data2 = pd.read_csv(f'{current_dir}/ckjm_output.csv')
+        # data1 = pd.read_csv("targ_files/ckjm_output.csv")
+        # data2 = pd.read_csv(f'{current_dir}/ckjm_output.csv')
+        #
+        # merged_output = pd.merge(data2, data1, on='TARGET_CLASS', how='outer')
+        # merged_output.to_csv(f'{current_dir}/merged_output.csv')
+        # return redirect(f"/result/{current_id}")
 
-        merged_output = pd.merge(data2, data1, on='TARGET_CLASS', how='outer')
-        merged_output.to_csv(f'{current_dir}/merged_output.csv')
-        return redirect(f"/result/{current_id}")
+        return redirect("/")
     return render_template("home.html")
 
 
@@ -90,4 +139,4 @@ def result(id):
         return render_template("result.html", csv=reader)
 
 if __name__ == '__main__':
-  app.run(port=8000)
+  app.run(port=8001)
